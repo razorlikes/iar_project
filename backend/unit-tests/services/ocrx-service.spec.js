2474ccc3abb96e90f795958f983945873a0e60f6
Jeremy Nieth <jnieth@live.de>
2022-12-19 21:31:04 +0100

    assignment 5.2: simple tests for ocrx-service.js provided

---
+++ backend/unit-tests/services/ocrx-service.spec.js
@@ -0,0 +1,34 @@
+const service = require("../../src/services/ocrx-service");
+const chai = require("chai");
+const sinon = require("sinon");
+const axios = require("axios");
+const expect = chai.expect;
+
+describe("Testing OpenCRX Service", () => {
+    afterEach(function () {
+        sinon.restore();
+    });
+
+    it("should return product name", async () => {
+        sinon.stub(axios, "get").resolves({
+            data: {
+                name: 'Cool Product Name',
+            }
+        });
+
+        expect(await service.getProductById({id: 1})).to.eqls({"name":"Cool Product Name"});
+    });
+
+    it("should return company info", async () => {
+        sinon.stub(axios, "get").resolves({
+            data: {
+                '@href': 'https://sepp-crm.inf.h-brs.de/opencrx-rest-CRX/org.opencrx.kernel.account1/provider/CRX/segment/Standard/account/USERIDHERE',
+                name: 'Cool Company Name',
+                accountRating: 9001,
+            }
+        });
+
+        expect(await service.getCustomerById({id: 1})).to.eqls({"id":"USERIDHERE","fullName":"Cool Company Name","customerRating":9001});
+    });
+})
+
